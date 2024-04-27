140008dc3be13143935412ea529c436b693aef45
MastrCheef99 <120070513+MastrCheef99@users.noreply.github.com>
2024-04-27 15:42:10 -0400

    added practice skipped detection
    i know i'll have to rewrite the entire system with online accounts but for now this is just local. next is detecting what day in the 64 day cycle you are in

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-function currentDay() {
-        var olddate = localStorage.getItem("thing")
+function started() {
+        var olddate = localStorage.getItem("thing");
         const date = new Date();
         const lastlogin = new Date(olddate)
+	currentYear = date.getFullYear();
+	currentMonth = date.getMonth();
+	currentDay = date.getDate();
+	pastYear = lastlogin.getFullYear();
+	pastMonth = lastlogin.getMonth();
+	pastDay = lastlogin.getDate();
+	console.log(currentYear + " " + currentMonth + " " + currentDay + " " + pastYear + " " + pastMonth + " " + pastDay + " ")
         if(date >= lastlogin) {
-                console.log("welcome");
+		window.alert("welcome");
+		console.log(date + " " + lastlogin + " ")
+		if (currentYear > pastYear) {
+                if (currentDay !== 1) {
+                        daySkip();
+                }
+        } else if (currentMonth > pastMonth) {
+                if (currentDay !== 1) {
+                daySkip();
+                }
+        } else if (currentDay > pastDay) {
+                if (currentDay - 1 !== pastDay) {
+                daySkip();
+   		}
+        } else if (currentDay == pastDay) {
+		window.alert("You already did your cards today, idiot");
+	}
         }else{
-                console.log("whomp whomp");
-                console.log(lastlogin);
-                console.log(olddate);
-                console.log(date);
+                console.log("your time settings are wrong");
         }
         localStorage.setItem("thing", date);
 }
-currentDay();
-
+function daySkip() {
+	window.alert("You skipped practice :(")
+}
+started();
