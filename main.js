17f3476fc22b8216a828d320d4baa129f98b0fb3
MastrCheef99 <120070513+MastrCheef99@users.noreply.github.com>
2024-04-27 13:31:40 -0400

    made the date comparison
    i'm kind of just gonna leave it here because i forgot why i need it anyway

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function currentDay() {
-	const date = new Date();
-	var day
-	var month
-	var year
-	day = date.getDate();
-	month = date.getMonth();
-	month += 1
-	year = date.getFullYear();
-	window.alert(month + "/" + day + "/" + year);
-	var dy = JSON.stringify(day)
-	window.alert(dy+1)
-	var d = JSON.parse(dy)
-	window.alert(d+1)
+        var olddate = localStorage.getItem("thing")
+        const date = new Date();
+        const lastlogin = new Date(olddate)
+        if(date >= lastlogin) {
+                console.log("welcome");
+        }else{
+                console.log("whomp whomp");
+                console.log(lastlogin);
+                console.log(olddate);
+                console.log(date);
+        }
+        localStorage.setItem("thing", date);
 }
 currentDay();
+
